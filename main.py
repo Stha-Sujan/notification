695d08daf2f22d16b07b29fa40be559c030fdec7
Sujan Shrestha <61510706+Stha-Sujan@users.noreply.github.com>
2022-03-03 22:19:07 +0545

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from plyer import notification
 if __name__ == "__main__":
     while True:
         notification.notify(
-            title = "Hi Sujan, Time to be hydrated",
+            title = "Hi (username), Time to be hydrated",
             message = "Drinking enough water each day is crucial for many reasons: to regulate body temperature, keep joints lubricated, prevent infections, deliver nutrients to cells, and keep organs functioning properly. Being well-hydrated also improves sleep quality, cognition, and mood.",
-            app_icon = r"/home/sujan/Desktop/lockdown/notification/icon.ico",
+            app_icon = r"path for icon.ico",
             timeout = 60
         )
         time.sleep(1200)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
